1b1c82784400c6bcacdad0826c102bf0a713a890
Achouak Cherif <65472852+Achwak28@users.noreply.github.com>
2023-10-26 01:00:43 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -5,3 +5,8 @@ python -m spacy download en_core_web_sm
 
 # Download NLTK dataset
 python -m nltk.downloader words
+
+pip install nltk
+pip install spacy==2.3.5
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
+pip install pyresparser
